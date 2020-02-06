ab59f155cb8eb2b9191c845a72767b277d27d5aa
rakannimer <rakannimer@gmail.com>
2020-02-06 12:42:54 +0200

    shadow theme

---
+++ src/gatsby-theme-docz/theme/index.js
@@ -0,0 +1,17 @@
+import baseTheme from "gatsby-theme-docz/src/theme/index";
+
+// import * as modes from "gatsby-theme-docz/src/theme/modes";
+
+// const theme = {
+//   ...baseTheme,
+//   colors: {
+//     ...baseTheme.colors,
+//     background: "green",
+//     modes: {
+//       dark: modes.dark,
+//       light: modes.light
+//     }
+//   }
+// };
+
+export default baseTheme;
